80b75f314538499652078cf6944ad5adb4753557
Dmitri Avdeev <d.avdeev@sdventures.com>
2022-06-08 12:26:39 +0300

    Initial

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Gems.Identity;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var longIds = File.ReadAllLines("D:\\mem.txt");
+            SimpleIdentityConverter c = new SimpleIdentityConverter();
+            var strIds = new List<string>();
+            foreach (var longId in longIds)
+            {
+                strIds.Add($"{c.Convert(long.Parse(longId))}");
+            }
+
+            File.WriteAllLines("D:\\id_conve.txt", strIds);
+
+            var existedStrIs = File.ReadAllLines("D:\\existed.txt");
+
+            var notExisted = strIds.Except(existedStrIs).ToList();
+
+            File.WriteAllLines("D:\\notExisted.txt", notExisted);
+
+
+
+
+        }
+    }
+}
---
+++ Program1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("D:\\Expr.csv");
+            var sqls = new List<string>();
+            foreach (var line in lines)
+            {
+                string[] idExpr = line.Split(',');
+                sqls.Add($"INSERT INTO [MCS.Commissions].[Expressions]([AgencyId],[AgencyFamily],[Expression]) VALUES('{idExpr[0]}', 'cp', '{idExpr[1]}')");
+            }
+
+            File.WriteAllLines("D:\\Expr.sql", sqls);
+        }
+    }
+}
